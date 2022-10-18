02f9b750cb5abc0f358a607088898a2163452bda
b34u <beauhelmi@gmail.com>
2022-10-18 11:54:36 +0800

    conditionally input loading messages

--- src/Home.js
+++ src/Home.js
@@ -2,30 +2,37 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState (null);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
-    const [name, setName] = useState('mario');
+  // const [name, setName] = useState('mario');
 
-    const handleDelete = (id) => {
-      const newBlogs = blogs.filter(blog => blog.id !== id );
-      setBlogs(newBlogs);
-    };
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter(blog => blog.id !== id );
+  //   setBlogs(newBlogs);
+  // };
 
-    useEffect(( ) => {
-      fetch('http://localhost:8001/blogs')
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        setBlogs(data);
-      })
-    }, [] );
+  useEffect(() => {
+   setTimeout(()  => {
+    fetch('http://localhost:8001/blogs')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      setBlogs(data);
+      setIsPending(false);
+    })
+   }, 1000);
+  }, []);
 
-    return (
-      <div className="home">
-        {blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />}
-      </div>
-    );
+  return (
+    <div className="home">
+      { isPending && <div>Loading...</div> }
+      {blogs && <BlogList blogs={blogs} title="All blogs" />}
+
+    </div>
+
+  );
 }
 
 export default Home;
